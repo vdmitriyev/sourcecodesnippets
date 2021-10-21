0ac5175bcfa02e926c3e8999c0ec2a06a86361c9
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2021-10-21 21:29:14 +0200

    ADD: Command prompt before re-initialization of virtual env

--- python/windows/cmdInitiateEnv.bat
+++ python/windows/cmdInitiateEnv.bat
@@ -1,5 +1,14 @@
 @echo off
+setlocal
+:PROMPT
+SET AREYOUSURE=N
+SET /P AREYOUSURE=Do you want to create new virtual environment (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
 SET PATH=C:\Compilers\Python38\Scripts\;C:\Compilers\Python38\;%PATH%
 python -m venv .venv
 call .\.venv\Scripts\activate.bat
 pip install -r requirements.txt
+
+:END
+endlocal
