c4a9ee78fb45928b035a20d9c4fcc6f26475e948
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2022-05-19 11:08:22 +0200

    UPDATE: post-save hook for jupyter notebooks

--- python/jupyter_notebook_config_post_save.py
+++ python/jupyter_notebook_config_post_save.py
@@ -1,10 +1,3 @@
-################################################
-#
-# Own hooks
-#
-################################################
-
-c = get_config()
 
 def post_save(model, os_path, contents_manager):
     '''Post-save hook to automatically convert notebooks to .py scripts and .html files
@@ -23,16 +16,18 @@ def post_save(model, os_path, contents_manager):
     def convert_and_copy(target_format, os_path):
         '''Convert and copy'''
 
-        if target_format not in ['html', 'py']:
+        nbconvert_mapping = {'html': 'html', 'py' : 'python'}
+        if target_format not in nbconvert_mapping:
             print('[x] Wrong format for conversion')
             return
 
         d, fname = os.path.split(os_path)
         fname_target = f'{fname[:-6]}.{target_format.lower()}'
+
         dir_target = os.path.join(d, target_format.upper())
-        if not os.path.exists(dir_target):
-            os.makedirs(dir_target)
-        check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
+        if not os.path.exists(dir_target): os.makedirs(dir_target)
+
+        check_call(['jupyter', 'nbconvert', '--to', nbconvert_mapping[target_format], fname], cwd=d)
         shutil.move(os.path.join(d, fname_target), os.path.join(dir_target, fname_target))
         print(f'{datetime.now()} save into {os.path.join(dir_target, fname_target)}')
 
